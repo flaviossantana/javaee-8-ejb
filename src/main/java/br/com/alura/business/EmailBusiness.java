9cd796c38f7341ef1a83c777d3410ddfbd004f18
Flávio Santana <9090flavio@gmail.com>
2020-08-18 20:19:53 -0300

    [EJB COM JAVA EE 8: API REST COM WILDFLY 15][INTRODUÇÃO AO EJB][CRIAÇÃO DO PROJETO E O PRIMEIRO EJB]

---
+++ src/main/java/br/com/alura/business/EmailBusiness.java
@@ -0,0 +1,17 @@
+package br.com.alura.business;
+
+
+import javax.ejb.Stateless;
+import java.util.Arrays;
+import java.util.List;
+
+@Stateless
+public class EmailBusiness {
+
+
+    public List<String> agendamentos(){
+        return Arrays.asList("email-01@foofle.com", "email-02@foofle.com");
+    }
+
+
+}
---
+++ src/test/java/br/com/alura/business/EmailBusinessTest.java
@@ -0,0 +1,8 @@
+package br.com.alura.business;
+
+
+public class EmailBusinessTest {
+    public static void main(String[] args) {
+        System.out.println("Classe de teste!");
+    }
+}

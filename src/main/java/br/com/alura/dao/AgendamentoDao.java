622db97a4166fa7a8c5fcc5e2409373713cf5396
Flávio Santana <9090flavio@gmail.com>
2020-09-02 08:01:47 -0300

    [EJB COM JAVA EE 8: API REST COM WILDFLY 15][INTRODUÇÃO AO EJB][USANDO ENTITYMANAGER]

--- src/main/java/br/com/alura/dao/AgendamentoDao.java
+++ src/main/java/br/com/alura/dao/AgendamentoDao.java
@@ -4,13 +4,12 @@ import br.com.alura.entity.Agendamento;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Stateless
 public class AgendamentoDao {
 
-    @PersistenceContext
+//    @PersistenceContext
     private EntityManager manager;
 
     public List<Agendamento> todos(){
--- src/main/java/br/com/alura/resource/EmailResource.java
+++ src/main/java/br/com/alura/resource/EmailResource.java
@@ -6,8 +6,12 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.concurrent.TimeUnit;
 
 @Path("/email")
 public class EmailResource {
@@ -20,5 +24,25 @@ public class EmailResource {
     public Response todos(){
         return Response.ok(emailBusiness.todos()).build();
     }
+
+    @GET
+    @Path("http")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response http(){
+
+        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+        clientBuilder.connectTimeout(10000, TimeUnit.MILLISECONDS);
+        clientBuilder.readTimeout(10000, TimeUnit.MILLISECONDS);
+
+        Client client = clientBuilder.build();
+
+        WebTarget target = client.target("https://run.mocky.io");
+        String retorno = target.path("/v3/8349b5f0-1156-4139-9ffa-aa8027111200").request().get(String.class);
+
+        return Response.ok(retorno).build();
+
+    }
+
+
 
 }
